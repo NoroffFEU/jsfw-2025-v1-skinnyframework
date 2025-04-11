042fe6f17217a8b886145147d8e7eea11c142fef
skinnyK <skinnykdev@gmail.com>
2025-04-11 13:50:45 +0200

    removed test code and drilled in themeStyles

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -8,41 +7,14 @@ import Footer from "./components/Footer";
  * The heading changes color on hover between red and green.
  * @returns {JSX.Element} The Layout component structure
  */
-const Layout = () => {
-  const [hover, setHover] = useState(false);
-  /**
-   * Handles mouse enter event on the layout heading
-   * Sets the hover state to true
-   */
-  function handleMouseEnter() {
-    setHover(true);
-  }
-  /**
-   * Handles mouse leave event on the layout heading
-   * Sets the hover state to false
-   */
-  function handleMouseLeave() {
-    setHover(false);
-  }
-  /**
-   * Effect hook that changes the layout heading color based on hover state
-   * Changes to red when hovered, green when not hovered
-   */
-  useEffect(() => {
-    const color = hover ? "red" : "green";
-    const layoutHeading = document.getElementById("layoutHeading");
-    if (layoutHeading) {
-      layoutHeading.setAttribute("style", `color: ${color}`);
-    }
-  }, [hover]);
+const Layout = ({ themeStyles }) => {
 
   return (
     <>
       <Header />
       <h1
         id="layoutHeading"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
+        className={themeStyles.heading}
       >
         LAYOUT
       </h1>
