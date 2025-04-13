72fa862d1cc9a68672a1113f0927279de181a9ce
skinnyK <skinnykdev@gmail.com>
2025-04-13 10:55:35 +0200

    quick format prettier

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,14 +2,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 const Layout = ({ themeStyles }) => {
-
   return (
     <>
       <Header themeStyles={themeStyles} />
-      <h1
-        id="layoutHeading"
-        className={themeStyles.heading}
-      >
+      <h1 id="layoutHeading" className={themeStyles.heading}>
         LAYOUT
       </h1>
       <button>a button</button>
