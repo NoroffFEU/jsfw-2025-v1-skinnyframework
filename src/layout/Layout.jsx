c95ba05439e2028fdf30a1521ea329205c806619
skinnyK <skinnykdev@gmail.com>
2025-04-13 09:07:49 +0200

    formatting

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-/**
- * Layout component that provides the basic structure of the application.
- * Includes a header, interactive heading, and footer.
- * The heading changes color on hover between red and green.
- * @returns {JSX.Element} The Layout component structure
- */
 const Layout = ({ themeStyles }) => {
 
   return (
