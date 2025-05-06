83f167074889543f54041b07044ed8167b1ef5f0
skinnyK <skinnykdev@gmail.com>
2025-05-06 20:03:57 +0200

    rearranged routes to make more sense in the header

--- src/routes.tsx
+++ src/routes.tsx
@@ -9,9 +9,9 @@ import themeStyles from './styles/theme.module.css';
 
 const routes = [
     { path: '/', element: <Home themeStyles={themeStyles} />, showInNav: true, }, // Home page
-    { path: '/product/:id', element: <Product themeStyles={themeStyles} />, showInNav: false }, // Product page
-    { path: '/checkout', element: <Checkout themeStyles={themeStyles} />, showInNav: true, }, // Checkout page
     { path: '/contact', element: <Contact themeStyles={themeStyles} />, showInNav: true }, // Contact page
+    { path: '/checkout', element: <Checkout themeStyles={themeStyles} />, showInNav: true, }, // Checkout page
+    { path: '/product/:id', element: <Product themeStyles={themeStyles} />, showInNav: false }, // Product page
     { path: '/cart', element: <Cart themeStyles={themeStyles} />, showInNav: false, }, // Cart page
     { path: '*', element: <ErrorPage themeStyles={themeStyles} />, showInNav: false }, // Catch-all route for 404
   ]
