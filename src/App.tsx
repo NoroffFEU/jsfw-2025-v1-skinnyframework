7946db35ab3b3b161254d123835600b0654088e9
skinnyK <skinnykdev@gmail.com>
2025-05-06 19:59:56 +0200

    moved routes to seperate file to help with readablity

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Layout from './layout/Layout';
-// pages
-import Home from './pages/Home';
-import Product from './pages/Product';
-import Cart from './pages/Cart';
-import Checkout from './pages/Checkout';
-import Contact from './pages/Contact';
-import ErrorPage from './pages/ErrorPage';
-import themeStyles from './styles/theme.module.css';
 import { FC } from 'react';
 import { PageProps } from 'types/props';
-
-const routes = [
-  { path: '/', element: <Home themeStyles={themeStyles} />, showInNav: true, }, // Home page
-  { path: '/product/:id', element: <Product themeStyles={themeStyles} />, showInNav: false }, // Product page
-  { path: '/checkout', element: <Checkout themeStyles={themeStyles} />, showInNav: true, }, // Checkout page
-  { path: '/contact', element: <Contact themeStyles={themeStyles} />, showInNav: true }, // Contact page
-  { path: '/cart', element: <Cart themeStyles={themeStyles} />, showInNav: false, }, // Cart page
-  { path: '*', element: <ErrorPage themeStyles={themeStyles} />, showInNav: false }, // Catch-all route for 404
-];
+import routes from './routes';
 
 const App: FC<PageProps> = ({ themeStyles }) => {
   return (
