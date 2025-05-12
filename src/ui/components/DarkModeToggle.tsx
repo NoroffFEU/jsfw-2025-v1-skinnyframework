afbb6b54213c3572e5873098a80b524ce6568d01
skinnyK <skinnykdev@gmail.com>
2025-05-12 23:15:57 +0200

    implemented new dark-toggle-svg

--- src/ui/components/DarkModeToggle.tsx
+++ src/ui/components/DarkModeToggle.tsx
@@ -24,13 +24,11 @@ const DarkModeToggle: FC<DarkModeToggleProps> = ({ themeStyles }) => {
     <div className={themeStyles.darkModeToggle}>
       <button
         onClick={handleThemeSwitch}
-        className={themeStyles.darkModeToggleBtn}
-      >
+        className={themeStyles.darkModeToggleBtn}>
         <svg
           style={svgStyles}
           viewBox="0 0 100 100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+          xmlns="http://www.w3.org/2000/svg">
           {/* Sun Icon – visible in light mode */}
           <g
             id="sunIcon"
@@ -39,8 +37,7 @@ const DarkModeToggle: FC<DarkModeToggleProps> = ({ themeStyles }) => {
               transform: isDarkMode ? 'scale(0.5)' : 'scale(1)',
               transition:
                 'opacity 0.5s ease-in-out, transform 0.5s ease-in-out',
-            }}
-          >
+            }}>
             <circle cx="50" cy="50" r="25" fill="#FFD700" />
             <g stroke="#FFD700" strokeWidth="4">
               <line x1="50" y1="10" x2="50" y2="25" />
@@ -62,8 +59,7 @@ const DarkModeToggle: FC<DarkModeToggleProps> = ({ themeStyles }) => {
               transform: isDarkMode ? 'scale(1)' : 'scale(0.5)',
               transition:
                 'opacity 0.5s ease-in-out, transform 0.5s ease-in-out',
-            }}
-          >
+            }}>
             <path
               d="M60 50a25 25 0 1 1-20-40 20 20 0 0 0 20 40z"
               fill="#D0D0D0"
