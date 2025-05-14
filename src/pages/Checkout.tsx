b88c32515be998358e09cb0ecb239004915b9adf
skinnyK <skinnykdev@gmail.com>
2025-05-14 20:57:31 +0300

    set up checkout page to show thank you for order message and added button to navigate back to home page.

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,12 +1,32 @@
 import { PageProps } from '../types/props';
 import { FC } from 'react';
 import Wrapper from '../ui/components/Wrapper';
+import { useCart } from '../context/CartContext';
+import { useNavigate } from 'react-router';
+import { useEffect } from 'react';
 
 const Checkout: FC<PageProps> = ({ themeStyles }) => {
+  const { clearCart } = useCart();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    clearCart();
+  }, []);
+
   return (
     <Wrapper themeStyles={themeStyles}>
       <>
         <h1 className={themeStyles.heading}>CHECKOUT</h1>
+        <div>
+          <p className={themeStyles.text}>
+            Thank you for your order! Your order has been placed successfully.
+          </p>
+          <button
+            className={themeStyles.button}
+            onClick={() => navigate('/')}>
+            Spend some more money
+          </button>
+        </div>
       </>
     </Wrapper>
   );
